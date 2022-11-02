c6c8e7a854b4850c16793ef6075234364836dbc3
gammx <dakdaker55@gmail.com>
2022-11-01 17:20:24 -0700

    update: add marquee section

---
+++ src/components/Marquee.tsx
@@ -0,0 +1,21 @@
+import type React from 'react';
+import MarqueePrimitive from "react-fast-marquee";
+import cn from 'classnames';
+
+interface MarqueeProps extends React.HTMLProps<HTMLDivElement> {
+  direction?: 'left' | 'right';
+}
+
+const Marquee: React.FC<MarqueeProps> = ({ className, direction = 'left', children }) => {
+  return (
+    <div className="overflow-hidden">
+      <MarqueePrimitive gradient={false} direction={direction} speed={150} className="overflow-hidden">
+        <p className={cn("text-9xl flex [&_*]:ml-16", className)}>
+          {children}
+        </p>
+      </MarqueePrimitive>
+    </div>
+  );
+};
+
+export default Marquee;
